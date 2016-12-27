d9414f0dd726451ec4e2bbfcbb44ce6c8ceb4d6d
Shota Yamamota <1230124fw@gmail.com>
2016-12-28 04:01:28 +0900

    make create new

--- app/controllers/restaurant_controller.rb
+++ app/controllers/restaurant_controller.rb
@@ -2,7 +2,23 @@ class RestaurantController < ApplicationController
   def top
     times = Time.zone.now
     @time =  times.strftime("%H:%M")
+  end
 
+  def new
+  end
+
+  def create
+    @restaurant = Restaurant.new
+    @restaurant.name = params[:name]
+    @restaurant.url = params[:url]
+    @restaurant.start_time=params[:start_time]
+    @restaurant.end_time=params[:end_time]
+    @restaurant.location = params[:location]
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant.id)
+  end
+  def edit
+    @restaurant = Restaurant.find(params[:id])
   end
   def index
     #  @restaurants = Restaurant.all
@@ -14,5 +30,7 @@ class RestaurantController < ApplicationController
   def show
     @restaurant = Restaurant.find(params[:id])
   end
+
+
 
 end
